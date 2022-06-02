fecc5dbf429b6a9be499a8351b236166077c0a12
Tuan Dinh <tuan.dinh@teqie.io>
2022-06-02 22:18:07 +0700

    create model

--- backend/models.py
+++ backend/models.py
@@ -1,29 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
 class User(models.Model):
     id = models.IntegerField(primary_key=True, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
-    deleted_at = models.DateTimeField(null=True, blank=True)
-    email = models.EmailField(unique=True)
-    name = models.CharField(null=True, blank=True, max_length=100)
-    time_zone = models.CharField(null=False, max_length=100)
-    is_superadmin = models.BooleanField(default=False)
+    discord_user_id = models.CharField(max_length=18)
+    user_name = models.TextField(null=True)
 
     class Meta:
         db_table = 'users'
 
 
-class Report(models.Model):
+class post(models.Model):
     id = models.IntegerField(primary_key=True, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
-    work_done = models.TextField(null=True)
-    work_will_do = models.TextField(null=True)
-    report_content = models.TextField(null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_create = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
+    status = models.TextField()
+    id_channel = models.CharField(max_length=18)
+    do_yesterday = models.TextField()
+    do_today = models.TextField()
+    content = models.TextField()
+    time_post = models.DateTimeField()
+
+
 
     class Meta:
-        db_table = 'reports'
+        db_table = 'post'
